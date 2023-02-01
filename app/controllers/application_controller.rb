2ecd3ccb5a76308606c651d83fc53a65167e6b89
rs2190 <122139610+rs2190@users.noreply.github.com>
2023-02-01 11:39:00 +0000

    [Add] 2023/02/01 サインアップ,ログイン,ログアウト,top,about画面実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
+# deviseのコントローラは直接修正できないため、全てのコントローラに対する処理を行える権限を持つ、ApplicationControllerに記述する必要があります。
 class ApplicationController < ActionController::Base
+
+  # devise利用の機能（ユーザ登録、ログイン認証など）が使われる前に、configure_permitted_parametersメソッドが実行されます。
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # deviseのストロングパラメータ。<br>configure_permitted_parametersメソッドでは、devise_parameter_sanitizer.permitメソッドを使うことでユーザー登録(sign_up)の際に、ユーザー名(name)のデータ操作を許可しています。(初期時は name を入力しても、データとして保存することはできない)
+  def configure_permitted_parameters
+
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
