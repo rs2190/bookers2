5e5803b82d2cdcc0eff9c5879aa78f765b736393
rs2190 <122139610+rs2190@users.noreply.github.com>
2023-02-04 08:49:21 +0000

    [Update] 2023/02/04 プロフィール画像が存在しない場合のデフォルト画像イメージ変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
   # ActiveStorageでプロフィール画像を保存できるように設定しました。
   has_one_attached :profile_image
 
+  # プロフィール画像のgetterメソッド。<br>画像サイズ指定可能(width(integer):横,height(integer):高さ)(px)
   def get_profile_image(width, height)
 
     # プロフィール画像が存在するか。
@@ -19,10 +20,12 @@ class User < ApplicationRecord
 
       # 存在しない場合
 
-      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
-      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+
     end
 
+    # 画像サイズの変更(px)
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
 
