f48382377ae20a1a260bbc949fafab67dfb4ae07
rs2190 <122139610+rs2190@users.noreply.github.com>
2023-02-09 04:29:45 +0000

    [Update] 2023/02/09 bodyバリデーション追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,8 +4,12 @@ class Book < ApplicationRecord
   # 1:N　=  userモデル：Bookモデル
    belongs_to :user
 
+   # title・・・空でないように設定
    validates :title, presence: true
-   validates :body, presence: true
 
+  # body・・・空でない、かつ最大200文字まで
+   validates :body,
+     presence: true,
+     length: { maximum: 200 }
 
 end
